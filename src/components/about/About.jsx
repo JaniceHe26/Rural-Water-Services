836186b21af1b0a3fd70977afc35124b7ed99b4e
Dominic Pitts <dom.r.pitts@gmail.com>
2020-05-25 13:11:50 -0400

    created about component
    about directory set up
    
    setup about as functiona component
    
    component about set up, img,title,p
    
    Update src/components/about/About.jsx
    
    Co-authored-by: Dominic Pitts <dom.r.pitts@gmail.com>
    
    Update src/components/about/About.jsx
    
    Co-authored-by: Dominic Pitts <dom.r.pitts@gmail.com>
    
    fixed export default

---
+++ src/components/about/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function About(){
+  return (
+    <React.Fragment>
+      <h4 className="aboutTitle">About</h4>
+
+      <div className="aboutDiv">
+        <img className="aboutImg" src="" alt="" title=""></img>
+        <p className="aboutText"></p>
+    
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default About;

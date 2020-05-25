50e5d3e0bfefbaae6ec3051ea5c12a5e964f873e
Janice <36203100+JaniceHe26@users.noreply.github.com>
2020-05-25 13:57:28 -0400

    Update src/components/about/About.jsx
    Co-authored-by: Dominic Pitts <dom.r.pitts@gmail.com>

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 export default function About(){
   return (
     <React.Fragment>
-    <h4 className="aboutTitle">About</h4>
+      <h4 className="aboutTitle">About</h4>
 
-    <div className="aboutDiv">
-    <img className="aboutImg" src="" alt="" title=""></img>
-    <p className="aboutText"></p>
+      <div className="aboutDiv">
+        <img className="aboutImg" src="" alt="" title=""></img>
+        <p className="aboutText"></p>
     
-    </div>
+      </div>
     </React.Fragment>
   )
 }

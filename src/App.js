98f18b37fc9bebada053a9719d8338300397e3cb
Janice <36203100+JaniceHe26@users.noreply.github.com>
2020-05-25 15:16:11 -0400

    About page commits (#2)
    * about directory set up
    
    * setup about as functiona component
    
    * component about set up, img,title,p
    
    * Update src/components/about/About.jsx
    
    Co-authored-by: Dominic Pitts <dom.r.pitts@gmail.com>
    
    * Update src/components/about/About.jsx
    
    Co-authored-by: Dominic Pitts <dom.r.pitts@gmail.com>
    
    * created about component
    
    about directory set up
    
    setup about as functiona component
    
    component about set up, img,title,p
    
    Update src/components/about/About.jsx
    
    Co-authored-by: Dominic Pitts <dom.r.pitts@gmail.com>
    
    Update src/components/about/About.jsx
    
    Co-authored-by: Dominic Pitts <dom.r.pitts@gmail.com>
    
    fixed export default
    
    * deleted files
    
    * p holder text added
    
    * css added for title and container
    
    * export default on the bottom
    
    * imported css to about jsx
    
    Co-authored-by: Janice He <aikeiti@dominics-mbp.myfiosgateway.com>
    Co-authored-by: Dominic Pitts <dom.r.pitts@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/header/Header';
+import About from './components/about/About';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <header>
         <Header />
       </header>
+      <About />
     </div>
   );
 }

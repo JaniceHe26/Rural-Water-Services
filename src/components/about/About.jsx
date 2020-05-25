6d57f11b33b5b94afff1441bd4747c2578afbda8
Janice <36203100+JaniceHe26@users.noreply.github.com>
2020-05-25 14:33:46 -0400

    about directory set up (#1)
    * about directory set up
    
    * setup about as functiona component
    
    * component about set up, img,title,p
    
    * Update src/components/about/About.jsx
    
    Co-authored-by: Dominic Pitts <dom.r.pitts@gmail.com>
    
    * Update src/components/about/About.jsx
    
    Co-authored-by: Dominic Pitts <dom.r.pitts@gmail.com>
    
    Co-authored-by: Janice He <aikeiti@dominics-mbp.myfiosgateway.com>
    Co-authored-by: Dominic Pitts <dom.r.pitts@gmail.com>

---
+++ src/components/about/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function About(){
+  return (
+    <React.Fragment>
+      <h4 className="aboutTitle">About</h4>
+
+      <div className="aboutDiv">
+        <img className="aboutImg" src="" alt="" title=""></img>
+        <p className="aboutText"></p>
+    
+      </div>
+    </React.Fragment>
+  )
+}

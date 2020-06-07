c7eeb739f612abec1d83d24aa0d8a7ff255b85e6
Janice <36203100+JaniceHe26@users.noreply.github.com>
2020-06-07 18:02:06 -0400

    Menu dev (#9)
    * fixed history header
    
    * few directories added
    
    * Menu added/started
    
    * functional component w/ hooks
    
    * menu
    
    Co-authored-by: Janice He <aikeiti@dominics-mbp.myfiosgateway.com>
    Co-authored-by: Dominic Pitts <dom.r.pitts@gmail.com>

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,9 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Menu.css";
 
 function Menu() {
-
+  const [services, setServices] = useState("")
 }
-
 
 export default Menu; 

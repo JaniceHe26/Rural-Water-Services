a4b9f545177913133ce201ede60b1a992df4040e
Janice <36203100+JaniceHe26@users.noreply.github.com>
2020-06-20 15:46:11 -0400

    divider started/added (#11)
    * divider started/added
    
    * divider margin
    
    Co-authored-by: Janice He <aikeiti@dominics-mbp.myfiosgateway.com>

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import About from './components/about/About';
 import Header from './components/header/Header';
 import History from './components/history/History';
+import Divider from '../src/shared/components/Divider';
 import './App.css';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       </header>
       <body className="BodyContent">
         <About />
+        <Divider />
         <History />
       </body>
     </div>

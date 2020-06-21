ed6aed85bda20f2a7fe6c829af5d9636fd6613c0
Janice He <aikeiti@dominics-mbp.myfiosgateway.com>
2020-06-20 20:30:18 -0400

    restyled and started hero

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Hero.css";
+
+function Hero() {
+  return(
+  <div className="HeroContainer">
+    <h3>Providing 20 years of services for locals</h3>
+  </div>
+  )
+}
+
+export default Hero; 

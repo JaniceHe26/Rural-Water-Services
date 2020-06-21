d61039d504a980d649d82adc99d6232bebbc355f
Janice He <aikeiti@dominics-mbp.myfiosgateway.com>
2020-06-21 17:50:44 -0400

    data json

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
       <div className="FooterContainer">
         <div className="Text">Rural Water Services</div>
         <div className="Text">Email: ruralwater6@gmail.com</div>
-        <div className="Text">Phone: (913)238-0876</div>
+        <div className="Text"><i class="fa fa-phone-square" aria-hidden="true">Phone: (913)238-087</i></div>
         <div className="Text">Location: Wellsville, Kansas</div>
       </div>
     </React.Fragment>

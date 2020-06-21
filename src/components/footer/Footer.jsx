834a3de47ba064dce23e17ffbd9c346025d6c5ce
Dominic Pitts <dom.r.pitts@gmail.com>
2020-06-21 17:51:01 -0400

    updated footer colors

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,10 +5,10 @@ function Footer() {
   return (
     <React.Fragment>
       <div className="FooterContainer">
-        <div className="Text">Rural Water Services</div>
-        <div className="Text">Email: ruralwater6@gmail.com</div>
-        <div className="Text">Phone: (913)238-0876</div>
-        <div className="Text">Location: Wellsville, Kansas</div>
+        <div>Rural Water Services</div>
+        <div>Email: ruralwater6@gmail.com</div>
+        <div><i class="fa fa-phone" aria-hidden="true"></i>&nbsp; (913)238-0876</div>
+        <div>Location: Wellsville, Kansas</div>
       </div>
     </React.Fragment>
   );
